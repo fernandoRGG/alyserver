27f52f692bcb05c4e413b4d053fd5e9edd904f33
fernando garcia <fernando.garcia131103@gmail.com>
2025-02-22 01:44:42 -0600

    Subiendo mi proyecto

---
+++ server.js
@@ -0,0 +1,22 @@
+const WebSocket = require('ws');
+const port = 8081;  // Usa el puerto de Render o 8080 si estás en localprocess.env.PORT ||
+const wss = new WebSocket.Server({ port });
+let clients = [];
+wss.on('connection', (ws) => {
+    console.log('Nuevo cliente conectado');
+    clients.push(ws);
+    ws.on('message', (message) => {
+        console.log('Mensaje recibido:', message);
+        // Reenviar el mensaje a todos los clientes conectados
+        clients.forEach(client => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(message);
+            }
+        });
+    });
+    ws.on('close', () => {
+        console.log('Cliente desconectado');
+        clients = clients.filter(client => client !== ws);
+    });
+});
+console.log(`Servidor WebSocket iniciado en ws://localhost:${port}`);
